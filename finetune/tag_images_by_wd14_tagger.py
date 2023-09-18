7477bd02de855a774f29ac93b1fa2b4505f22825
phamhungd <39170814+phamhungd@users.noreply.github.com>
2023-09-18 21:36:38 +0700

    Update tag_images_by_wd14_tagger.py

--- finetune/tag_images_by_wd14_tagger.py
+++ finetune/tag_images_by_wd14_tagger.py
@@ -2,7 +2,6 @@ import argparse
 import csv
 import glob
 import os
-import sys
 
 from PIL import Image
 import cv2
@@ -14,7 +13,6 @@ import torch
 from pathlib import Path
 
 import library.train_util as train_util
-sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 # from wd14 tagger
 IMAGE_SIZE = 448
 
